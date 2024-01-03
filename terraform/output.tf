136312db1c9123123450f52f7d06146d56431c9d
Richard Lee <richardlee@dicci.co.uk>
2024-01-03 20:59:44 +0000

    chore: destroy kube

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,3 @@
 output "subscription_id" {
   value = data.azurerm_client_config.current.subscription_id
 }
-
-output "cluster-id" {
-  value = digitalocean_kubernetes_cluster.kubernetes_cluster.id
-}
