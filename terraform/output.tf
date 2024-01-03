ace19a177f0f786135a898e357a792257120af33
Richard Lee <richardlee@dicci.co.uk>
2024-01-03 20:25:37 +0000

    feat: kubernetes

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,7 @@
 output "subscription_id" {
   value = data.azurerm_client_config.current.subscription_id
 }
+
+output "cluster-id" {
+  value = digitalocean_kubernetes_cluster.kubernetes_cluster.id
+}
